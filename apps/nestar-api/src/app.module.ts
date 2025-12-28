ba908a3540bb8210444dd5ddf978057d744b14c7
Marcus <mynewlife2778@gmail.com>
2025-12-28 18:38:27 +0900

    feat: create member resolver & service model

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,23 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import {GraphQLModule} from '@nestjs/graphql'
-import {ApolloDriver} from '@nestjs/apollo'
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 
-@Module({ // INTEGRATION
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({ 
-    driver: ApolloDriver,
-    playground: true,
-    uploads: false, // for receiving images, multipart/form-data is ON.
-    autoSchemaFile: true,
-
-  }), ComponentsModule, DatabaseModule, 
-],
-  controllers: [AppController], // For REST API
-  providers: [AppService, AppResolver],
+@Module({
+	// INTEGRATION
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false, // for receiving images, multipart/form-data is ON.
+			autoSchemaFile: true,
+		}),
+		ComponentsModule,
+		DatabaseModule,
+	],
+	controllers: [AppController], // For REST API
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
 
