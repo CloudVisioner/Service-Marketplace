b4340911c3196b74145c81b8eebd44d923dc82e8
Marcus <mynewlife2778@gmail.com>
2025-12-31 15:56:17 +0900

    fix: modify nest cli detail

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -1,11 +1,11 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import type { ObjectId } from 'mongoose';
+import  mongoose from 'mongoose';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
 
 @ObjectType()
 export class Member {
 	@Field(() => String)
-	_id: ObjectId;
+	_id: mongoose.ObjectId;
 
 
 
